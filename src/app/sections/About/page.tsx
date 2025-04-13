5df679c05a6c62cb51a45cca6657d46fc46059a6
“Mansour” <mansournosrati69@gmail.com>
2025-04-13 13:52:47 +0330

    third commit

--- src/app/sections/About/page.tsx
+++ src/app/sections/About/page.tsx
@@ -1,5 +1,9 @@
 export default function About () {
     return (
-        <h2>About 2</h2>
+        <div className="mt-[94px] flex flex-col items-center justify-start px-4 bg-black w-screen h-screen">
+            <span className="text-white/50 w-fit lg:py-2 py-1 lg:px-4 px-3 text-[10px] lg:text-[14px] font-medium bg-gray-700 rounded-[20px]  ">ABOUT LIFE</span>
+            <p className="text-[24px] font-semibold text-white text-center mt-[10px]">Empowering Innovation in Biotechnology</p>
+            <p className="text-white/50 text-justify indent-5 mt-[10px] text-[14px] leading-[24px] font-normal">LIFE Technologies is a global biotech ecosystem designed to bridge the gap between groundbreaking scientific research and real-world applications. By combining Web3 tools with advanced AI, we empower innovation, ensure transparency, and drive equitable access to life-saving solutions.</p>
+        </div>
     )
 }
