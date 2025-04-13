74e60ecabf277e419168c0973a692b2524e8b692
“Mansour” <mansournosrati69@gmail.com>
2025-04-13 14:24:17 +0330

    fourth commit

---
+++ src/app/Components/ImgCard.tsx
@@ -0,0 +1,7 @@
+export default function ImgCard () {
+    return (
+        <div className="p-6 mx-4 rounded-5 h-[623px] w-[358px] ">
+            <h2 className="text-white ">Hello</h2>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "./sections/home/Hero";
 import About from "./sections/about/page";
+import ImgCard from "./Components/ImgCard";
 
 export default function Home() {
   return (
    <div className="bg-gray-950">
     <Hero/>
     <About/>
+    <ImgCard/>
    </div>
   );
 }
